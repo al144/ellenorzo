7c5a2fd61f001bf465f5dfdcfddeafe8f7a328ae
al144 <davidasztral2007@gmail.com>
2025-10-04 15:15:38 +0200

    secert key

--- ellenorzo/settings.py
+++ ellenorzo/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t)rp^dr#+hr*he%r42bh!ocf89$p%-w4^zmji+&iea(h@4-+-3'
+from django.core.management.utils import get_random_secret_key
+
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
